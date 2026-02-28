b0e0017bd779ce9c2925f2b1e9a55f17132f90a5
Maikane-pro <bendoungaarmel@gmail.com>
2026-02-28 05:18:23 +0100

    Create app/config.php with database and site configuration

---
+++ app/config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'database' => [
+        'host' => 'localhost',
+        'username' => 'dbuser',
+        'password' => 'dbpassword',
+        'dbname' => 'dbname',
+    ],
+    'site' => [
+        'name' => 'My Site',
+        'url' => 'https://www.mysite.com',
+    ],
+];
