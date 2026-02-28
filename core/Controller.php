7da7c303b9983415e35688667da2979413135d6e
Maikane-pro <bendoungaarmel@gmail.com>
2026-02-28 05:04:36 +0100

    Create core/Controller.php file for base controller class

---
+++ core/Controller.php
@@ -0,0 +1,14 @@
+<?php
+
+class Controller {
+    // Base controller methods go here
+    public function __construct() {
+        // Initialization code
+    }
+
+    public function render($view, $data = []) {
+        // Code to render a view
+    }
+
+    // Additional methods
+}
